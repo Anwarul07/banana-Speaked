8eb9f5705594cdafd20132928fb248758a0636a4
Anwarul07 <mdanwarul064@gmail.com>
2023-09-04 11:09:44 +0530

    js
    completed js file

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
-// console  using directly
-console.log("console using directly");
-
-// console  using as variable
-var nya = "console using as a var ";
-console.log(nya);
-
-// alert directly
-
- alert("aler using directly");
-
-//alert as a variable
-var vara="this is alert using var"
-alert(vara);
-
-// I/O using directly
-prompt(" what is your  father's name "+"direct i/o method");
-alert("ok   direct method is sucess as ! Mr ");
-
-// I/O using as variable
-var ask = prompt(" what is your name  using var ");
-alert("ok   Permission Granted  using var ! Mr " + ask);
-
-var ask = prompt(" what is your Identity  var var");
-var reply= "ok   Permission Granted  var var ! Mr " + ask
+var ask = prompt(" what is your Identity Mr ?");
+var reply = "Ok Permission Granted! & Welcome Mr " + ask;
 alert(reply);
 
-var bttn = document.querySelector("#btn");
+var bttn = document.querySelector("#btnTranslate");
+var inputText = document.querySelector("#textinput");
+var outputbtn = document.querySelector("#btnOutput");
 bttn.addEventListener("click", clickHandler);
 
 function clickHandler() {
-  console.log("clicked!");
+  console.clear();
+  console.log(textinput.value);
+
+  // calling process
+
+  var inputText = textinput.value;
+
+  fetch(translationUrl(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      btnOutput.innerText = translatedText;
+    })
+    .catch(errorHandler);
+}
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function translationUrl(input) {
+  return serverURL + "?" + "text=" + input;
+}
+
+function errorHandler(error) {
+  console.log("Error !", error);
+  alert("Error please try again");
 }
